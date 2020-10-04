cac52505bc5a5eba3e4eae3ebf287dcf18b1bec1
Tsukanova Maria <cukanovaa93@gmail.com>
2020-10-04 14:08:58 +0300

    date: 4.10.2020

---
+++ prog/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using classlib;
+using System.IO;
+using System.Linq;
+using System.Threading;
+namespace prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo myDirectory = new DirectoryInfo(args.FirstOrDefault() ?? "images");
+            
+            MyClass obj = new MyClass(myDirectory);
+            var enterThread = new Thread(new ThreadStart(()=>
+            { 
+                while (Console.ReadKey().Key != ConsoleKey.Enter){ }
+                MyClass.cancelTokenSource.Cancel();
+            }));
+            enterThread.Start();
+            obj.GetResults();
+
+            foreach (var item in obj.result)
+            {
+                Console.Write(item);
+            }
+            System.Environment.Exit(0);
+        }
+    }
+}
