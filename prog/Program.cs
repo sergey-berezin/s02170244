aafd44a4b1b4533e2b67112e0b917b7b5d698165
Maria <48991614+marytsuk@users.noreply.github.com>
2020-10-07 19:50:24 +0300

    date: 4.10.2020 (#1)
    * date: 4.10.2020
    
    * date: 7.10.2020, fixed comments

---
+++ prog/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using classlib;
+using System.IO;
+using System.Linq;
+using System.Threading;
+namespace prog
+{
+    public class WriteToConsole: IConsoleView
+    {
+        public void ReturnRes(ImageInfo info)
+        {
+            Console.WriteLine("Predicting contents of image: ");
+            Console.Write(info);
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo myDirectory = new DirectoryInfo(args.FirstOrDefault() ?? "images");
+            
+            ImageRecognizer obj = new ImageRecognizer(myDirectory, new WriteToConsole());
+
+            var enterThread = new Thread(new ThreadStart(()=>
+            { 
+                while (Console.ReadKey().Key != ConsoleKey.Enter){ }
+                ImageRecognizer.cancelTokenSource.Cancel();
+            }));
+            enterThread.Start();
+            obj.GetResults();
+
+            // foreach (var item in obj.result)
+            // {
+            //     Console.WriteLine("Predicting contents of image...");
+            //     Console.Write(item);
+            //     Console.WriteLine();
+            // }
+            System.Environment.Exit(0);
+        }
+    }
+}
